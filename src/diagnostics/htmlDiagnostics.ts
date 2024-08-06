41e207e041aa8a2d82081e9bfef89c46eeace527
bpetermann <benjamin.petermann@gmx.at>
2024-08-06 22:05:04 +0200

    Add html missing elements array

--- src/diagnostics/htmlDiagnostics.ts
+++ src/diagnostics/htmlDiagnostics.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import { DomUtils, parseDocument } from 'htmlparser2';
 import { warnings } from './warnings';
+import { AnyNode } from 'domhandler';
 
 function getDiagnostic(
   node: any,
@@ -18,6 +19,54 @@ function getDiagnostic(
   return new vscode.Diagnostic(loc, message, vscode.DiagnosticSeverity.Warning);
 }
 
+function findDomElement(name: string, nodes: AnyNode[]) {
+  return nodes.find((node) => node && 'name' in node && node.name === name);
+}
+
+function checkElementsExists(
+  document: vscode.TextDocument,
+  domNodes: AnyNode[],
+  elements: [name: string, warning: string][]
+): vscode.Diagnostic[] {
+  return elements
+    .map(([name, warning]) => {
+      return !findDomElement(name, domNodes)
+        ? getDiagnostic({}, document, warning)
+        : null;
+    })
+    .filter((item) => item !== null);
+}
+
+function checkElement(
+  node: any,
+  attributeName: string,
+  message: string,
+  document: vscode.TextDocument
+) {
+  return node.attribs && !node.attribs[attributeName]
+    ? getDiagnostic(node, document, message)
+    : null;
+}
+
+function checkElementTags(
+  document: vscode.TextDocument,
+  domNodes: AnyNode[],
+  elements: { [element: string]: [attribute: string, warning: string] }
+) {
+  return domNodes
+    .map((node) => {
+      return 'name' in node && node.name in elements
+        ? checkElement(
+            node,
+            elements[node.name][0],
+            elements[node.name][1],
+            document
+          )
+        : null;
+    })
+    .filter((item) => item !== null);
+}
+
 export function getHtmlDiagnostics(
   text: string,
   document: vscode.TextDocument
@@ -27,25 +76,22 @@ export function getHtmlDiagnostics(
   try {
     const parsedDocument = parseDocument(text);
 
-    const checkElement = (
-      node: any,
-      elementName: string,
-      attributeName: string,
-      message: string
-    ) => {
-      if (
-        node.name === elementName &&
-        node.attribs &&
-        !node.attribs[attributeName]
-      ) {
-        diagnostics.push(getDiagnostic(node, document, message));
-      }
-    };
-
-    DomUtils.filter(
+    const domNodes = DomUtils.filter(
       (node) => node.type === 'tag',
       parsedDocument.children
-    ).forEach((node) => checkElement(node, 'html', 'lang', warnings.html.lang));
+    );
+
+    const missingTags = checkElementTags(document, domNodes, {
+      html: ['lang', warnings.html.lang],
+    });
+
+    const missingElements = checkElementsExists(document, domNodes, [
+      ['title', warnings.title.shouldExist],
+    ]);
+
+    [...missingTags, ...missingElements].forEach((diagnostic) =>
+      diagnostics.push(diagnostic)
+    );
   } catch (error) {
     console.error('Error parsing HTML: ', error);
   }
--- src/diagnostics/warnings.ts
+++ src/diagnostics/warnings.ts
@@ -6,4 +6,8 @@ export const warnings = {
     'aria-label':
       '[Refa11y] Button elements should have an aria-label for accessibility.',
   },
+  title: {
+    shouldExist:
+      "[Refa11y] Name your page using the <title> element to help screen reader users tell which page they're on",
+  },
 } as const;
